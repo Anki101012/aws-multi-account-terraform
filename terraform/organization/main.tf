8135e4050b38b3229653f0db2867b9a82fc2bcc1
Anki101012 <118600929+Anki101012@users.noreply.github.com>
2026-03-11 17:56:38 -0500

    Update main.tf

--- terraform/organization/main.tf
+++ terraform/organization/main.tf
@@ -3,10 +3,10 @@ data "aws_organizations_organization" "helixcloud_org" {
 
 resource "aws_organizations_organizational_unit" "dev" {
   name      = "dev"
-  id = data.aws_organizations_organization.helixcloud_org.roots[0].id
+  parent_id = data.aws_organizations_organization.helixcloud_org.roots[0].id
 }
 
 resource "aws_organizations_organizational_unit" "prod" {
   name      = "prod"
-  id = data.aws_organizations_organization.helixcloud_org.roots[0].id
+  parent_id = data.aws_organizations_organization.helixcloud_org.roots[0].id
 }
