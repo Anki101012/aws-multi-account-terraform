d0c56dc74fb19479f882bfdaa2bf650c755ddb8c
Anki101012 <118600929+Anki101012@users.noreply.github.com>
2026-03-11 17:55:00 -0500

    Update outputs.tf

--- terraform/organization/outputs.tf
+++ terraform/organization/outputs.tf
@@ -5,8 +5,8 @@ output "root_id" {
   value = data.aws_organizations_organization.helixcloud_org.roots[0].id
 }
 output "dev_ou_id" {
-  value = aws_organizations_organizational_unit.development.id
+  value = aws_organizations_organizational_unit.dev.id
 }
 output "prod_ou_id" {
-  value = aws_organizations_organizational_unit.production.id
+  value = aws_organizations_organizational_unit.prod.id
 }
