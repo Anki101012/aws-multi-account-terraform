85840284b043c1cfb0eb49a8ef21bb2af843e45a
Anki101012 <118600929+Anki101012@users.noreply.github.com>
2026-03-11 17:49:05 -0500

    Create variables.tf

---
+++ terraform/organization/variables.tf
@@ -0,0 +1,9 @@
+variable "dev_account_id" {
+  description = "development account"
+  type        = string
+}
+
+variable "prod_account_id" {
+  description = "production account"
+  type        = string
+}
